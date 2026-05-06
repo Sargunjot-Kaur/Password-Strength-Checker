7315d49a550c6ecb8a57a2a9879ff318f81f6542
Sargunjot-Kaur <sargunxsandhu@icloud.com>
2026-05-06 09:39:40 +0200

    updates in complexity check

--- checker.py
+++ checker.py
@@ -1,19 +1,17 @@
+
 def check_length(password):
-    characters_req = 12 - len(password)
-    if characters_req <= 0:
+    if len(password) >=12:
         return "Checked"
     else:
-        return "Length: Oops! Too short add {} more elements".format(characters_req)
+        return "Length: Oops! Too short like your dih"
     
 def check_complexity(password):
     # Return message if password contains at least 1 lowercase, 1 uppercase, 1 number, 1 symbol
-    
     has_lowercase = False
     has_uppercase = False
     has_numbers = False
     has_symbols = False
-    
-    complexity_symbols = [has_lowercase, has_uppercase, has_numbers, has_symbols]
+
     for char in password:
         if char.islower():
             has_lowercase = True
@@ -24,22 +22,19 @@ def check_complexity(password):
         elif char in "!@#$%^&*_-/?~":
             has_symbols = True
 
-    if has_lowercase and has_uppercase and has_numbers and has_symbols is True:
+    if has_lowercase and has_uppercase and has_numbers and has_symbols:
         return "Checked"
-     
-    if char.islower() is False:
-        return "add a lowercase element."
-    if char.isupper() is False:
-        return "add an uppercase element."
-    if char.isdigit() is False:
-        return "Add a number."
-    if char in "!@#$%^&*_-/?~" == False:
-        return "Add on of these !@#$%^&*_-/?~"
+    else:
+        missing=[]
 
-    
+    if has_lowercase == False:
+        missing.append("lower case")
+    if has_uppercase == False:
+        missing.append("Upper case")
+    if has_numbers == False:
+        missing.append("number")
+    if has_symbols == False:
+        missing.append("a symbol") 
 
-    
+        return "Missing:" + ", ".join(missing)           
 
-    
-          
-    
